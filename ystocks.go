05a06b30050365090f122c3b998e61ac3f3d259a
Colin Drake <colin.f.drake@gmail.com>
2012-08-24 19:24:43 -0400

    Add getProperty() and getProperties() along with tests

--- ystocks.go
+++ ystocks.go
@@ -1,5 +1,66 @@
+// Package ystocks implements a library for using the Yahoo! Finance stock
+// market API.
 package ystocks
 
+import (
+    "encoding/csv"
+    "net/http"
+    "strings"
+)
+
+// Constants with URL parts for API URL-building.
+const (
+    quotesBaseUrl = "http://download.finance.yahoo.com/d/quotes.csv?s="
+    staticUrl     = "&e=.csv"
+)
+
+// Stock symbol type; id must be a valid stock market symbol.
 type Stock struct {
     id string
+}
+
+type StockProperties [][]string
+
+const (
+    AfterHoursChangeRealtime = "c8"
+    AnnualizedGain           = "g3"
+    Ask                      = "a0"
+    // ...TODO...
+    Symbol                   = "s0"
+    // ...TODO...
+    Volume                   = "v0"
+)
+
+func (s *Stock) getProperty(prop string) (string, error) {
+    props, err := s.getProperties([]string{prop})
+
+    // Flatten properties to a single string if no error was found
+    if err == nil && len(props) == 1 && len(props[0]) == 1 {
+        return props[0][0], nil
+    }
+
+    return "", err
+}
+
+func (s *Stock) getProperties(props []string) (StockProperties, error) {
+    // Build up the Y! Finance API URL
+    propsStr := strings.Join(props, "")
+    url := quotesBaseUrl + s.id +
+           "&f=" + propsStr +
+           staticUrl
+
+    // HTTP GET the CSV data
+    resp, httpErr := http.Get(url)
+    if httpErr != nil {
+        return nil, httpErr
+    }
+
+    // Convert string CSV data to a usable data structure
+    reader := csv.NewReader(resp.Body)
+    records, parseErr := reader.ReadAll()
+    if parseErr != nil {
+        return nil, parseErr
+    }
+
+    return records, nil
 }
--- ystocks_test.go
+++ ystocks_test.go
@@ -1,12 +1,35 @@
 package ystocks
 
 import (
-  "testing"
+    "testing"
 )
 
+// Ensure the Stock type can be created successfully.
 func TestStockType(t *testing.T) {
-  s := Stock{"INTC"}
-  if s.id != "INTC" {
-    t.Error("Couldn't create Stock object")
-  }
+    s := Stock{"INTC"}
+    if s.id != "INTC" {
+        t.Error("Couldn't create Stock object")
+    }
+}
+
+// Ensure a single property can be grabbed from the API.
+func TestGetProperty(t *testing.T) {
+    s := Stock{"CSCO"}
+    sym, err := s.getProperty(Symbol)
+
+    if err != nil || sym != "CSCO" {
+        t.Error("Couldn't get Symbol property")
+    }
+}
+
+// Ensure multiple properties can be grabbed from the API.
+func TestGetProperties(t *testing.T) {
+    s := Stock{"MSFT"}
+    _, err := s.getProperties([]string{Symbol, Volume})
+
+    if err != nil {
+        t.Error("Couldn't get Symbol or Volume properties")
+    }
+
+    // TODO: test values returned from method
 }
